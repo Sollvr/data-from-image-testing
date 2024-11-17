e0381492245cb655a7576193753c599e257ab90f
numaanmk <numaanmk.nk@gmail.com>
2024-11-18 01:15:53 +0300

    last stages

--- middleware.ts
+++ middleware.ts
@@ -1,26 +1,30 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-
-export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession()
-
-  if (!session && req.nextUrl.pathname !== '/auth') {
-    return NextResponse.redirect(new URL('/auth', req.url))
-  }
-
-  if (session && req.nextUrl.pathname === '/auth') {
-    return NextResponse.redirect(new URL('/extract-data', req.url))
-  }
-
-  return res
-}
-
-export const config = {
-  matcher: ['/extract-data', '/auth'],
-} 
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (req.nextUrl.pathname.startsWith('/auth/callback')) {
+    return res
+  }
+
+  if (!session && req.nextUrl.pathname !== '/auth') {
+    return NextResponse.redirect(new URL('/auth', req.url))
+  }
+
+  if (session && req.nextUrl.pathname === '/auth') {
+    return NextResponse.redirect(new URL('/extract-data', req.url))
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ['/extract-data', '/auth', '/auth/callback'],
+} 
